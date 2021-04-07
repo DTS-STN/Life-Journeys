075bb96a61c61a9ef7ad672cacb2ba236e782a0c
MarcoGoC <marcogoc951@gmail.com>
2021-04-07 12:45:44 -0400

    deleted old components

--- components/cards/controls.js
+++
@@ -1,9 +0,0 @@
-export default function Controls() {
-  return (
-    <section className="text-3xl font-bold p-1 rounded-xl">
-      <button>show</button>
-      <button>hide</button>
-      <button>clear</button>
-    </section>
-  );
-}
--- components/controls/button.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Button({ text, route }) {
-  return (
-    <Link href={route}>
-      <button>{text}</button>
-    </Link>
-  );
-}
