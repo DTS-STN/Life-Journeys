56332ccb0090a6c004ae0789a2b990737e5c753c
MarcoGoC <marcogoc951@gmail.com>
2021-04-15 08:26:52 -0400

    adding starting code for share this page

---
+++ components/atoms/WbShare.js
@@ -0,0 +1,167 @@
+import React from "react";
+
+class WbShare extends React.Component {
+  render() {
+    return (
+      <div
+        className="wb-share wb-init wb-share-inited"
+        data-wb-share='{"lnkClass": "btn btn-default btn-block"}'
+        id="wb-auto-8"
+      >
+        <section
+          id="shr-pg0"
+          className="shr-pg mfp-hide modal-dialog modal-content overlay-def"
+        >
+          <header className="modal-header">
+            <h2 className="modal-title">Share this page</h2>
+          </header>
+          <div className="modal-body">
+            <ul className="list-unstyled colcount-xs-2">
+              <li>
+                <a
+                  href="https://bitly.com/a/bitmarklet?u=https%3A%2F%2Fwww.canada.ca%2Fen%2Fgovernment%2Fdept.html"
+                  className="shr-lnk bitly btn btn-default"
+                  target="_blank"
+                >
+                  bitly
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://www.blogger.com/blog_this.pyra?t=&amp;u=https%3A%2F%2Fwww.canada.ca%2Fen%2Fgovernment%2Fdept.html&amp;n=Departments%20and%20agencies%20-%20Canada.ca"
+                  className="shr-lnk blogger btn btn-default"
+                  target="_blank"
+                >
+                  Blogger
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://digg.com/submit?phase=2&amp;url=https%3A%2F%2Fwww.canada.ca%2Fen%2Fgovernment%2Fdept.html&amp;title=Departments%20and%20agencies%20-%20Canada.ca"
+                  className="shr-lnk digg btn btn-default"
+                  target="_blank"
+                >
+                  Digg
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://www.diigo.com/post?url=https%3A%2F%2Fwww.canada.ca%2Fen%2Fgovernment%2Fdept.html&amp;title=Departments%20and%20agencies%20-%20Canada.ca"
+                  className="shr-lnk diigo btn btn-default"
+                  target="_blank"
+                >
+                  Diigo
+                </a>
+              </li>
+              <li>
+                <a
+                  href="mailto:?to=&amp;subject=Departments%20and%20agencies%20-%20Canada.ca&amp;body=https%3A%2F%2Fwww.canada.ca%2Fen%2Fgovernment%2Fdept.html%0A"
+                  className="shr-lnk email btn btn-default"
+                  target="_blank"
+                >
+                  Email
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://www.facebook.com/sharer.php?u=https%3A%2F%2Fwww.canada.ca%2Fen%2Fgovernment%2Fdept.html&amp;t=Departments%20and%20agencies%20-%20Canada.ca"
+                  className="shr-lnk facebook btn btn-default"
+                  target="_blank"
+                >
+                  Facebook
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://mail.google.com/mail/?view=cm&amp;fs=1&amp;tf=1&amp;to=&amp;su=Departments%20and%20agencies%20-%20Canada.ca&amp;body=https%3A%2F%2Fwww.canada.ca%2Fen%2Fgovernment%2Fdept.html%0A"
+                  className="shr-lnk gmail btn btn-default"
+                  target="_blank"
+                >
+                  Gmail
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://www.linkedin.com/shareArticle?mini=true&amp;url=https%3A%2F%2Fwww.canada.ca%2Fen%2Fgovernment%2Fdept.html&amp;title=Departments%20and%20agencies%20-%20Canada.ca&amp;ro=false&amp;summary=&amp;source="
+                  className="shr-lnk linkedin btn btn-default"
+                  target="_blank"
+                >
+                  LinkedIn®
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://www.myspace.com/Modules/PostTo/Pages/?u=https%3A%2F%2Fwww.canada.ca%2Fen%2Fgovernment%2Fdept.html&amp;t=Departments%20and%20agencies%20-%20Canada.ca"
+                  className="shr-lnk myspace btn btn-default"
+                  target="_blank"
+                >
+                  MySpace
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://www.pinterest.com/pin/create/button/?url=https%3A%2F%2Fwww.canada.ca%2Fen%2Fgovernment%2Fdept.html&amp;media=&amp;description=Departments%20and%20agencies%20-%20Canada.ca"
+                  className="shr-lnk pinterest btn btn-default"
+                  target="_blank"
+                >
+                  Pinterest
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://reddit.com/submit?url=https%3A%2F%2Fwww.canada.ca%2Fen%2Fgovernment%2Fdept.html&amp;title=Departments%20and%20agencies%20-%20Canada.ca"
+                  className="shr-lnk reddit btn btn-default"
+                  target="_blank"
+                >
+                  reddit
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://www.tumblr.com/share/link?url=https%3A%2F%2Fwww.canada.ca%2Fen%2Fgovernment%2Fdept.html&amp;name=Departments%20and%20agencies%20-%20Canada.ca&amp;description="
+                  className="shr-lnk tumblr btn btn-default"
+                  target="_blank"
+                >
+                  tumblr
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://twitter.com/home?status=Departments%20and%20agencies%20-%20Canada.ca%20https%3A%2F%2Fwww.canada.ca%2Fen%2Fgovernment%2Fdept.html"
+                  className="shr-lnk twitter btn btn-default"
+                  target="_blank"
+                >
+                  Twitter
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://compose.mail.yahoo.com/?to=&amp;subject=Departments%20and%20agencies%20-%20Canada.ca&amp;body=https%3A%2F%2Fwww.canada.ca%2Fen%2Fgovernment%2Fdept.html%0A"
+                  className="shr-lnk yahoomail btn btn-default"
+                  target="_blank"
+                >
+                  Yahoo! Mail
+                </a>
+              </li>
+            </ul>
+            <p className="col-sm-12 shr-dscl">
+              No endorsement of any products or services is expressed or
+              implied.
+            </p>
+            <div className="clearfix"></div>
+          </div>
+        </section>
+        <a
+          href="#shr-pg0"
+          aria-controls="shr-pg0"
+          className="shr-opn wb-lbx btn btn-default btn-block wb-lbx-inited wb-init"
+          id="wb-auto-9"
+        >
+          <span className="glyphicon glyphicon-share"></span>Share this page
+        </a>
+      </div>
+    );
+  }
+}
+
+export default WbShare;
