bff81ce1ba66baac53bdd7dd92382a8876fb5424
P0NDER0SA <mikepond11@gmail.com>
2021-04-15 13:35:07 -0400

    adding file

---
+++ components/organisms/TopicBox.js
@@ -0,0 +1,25 @@
+//   /**
+//  *  Topic Box Component
+//  */
+// export function TopicBox(props) {
+//     return (
+//       <div className="topicbox">
+//         <p>
+//           Some stuff to checkout
+//           lsajf;sajfd;lsajfd;ksajd;lkfjsalkfjlksajkfhlksajhfdlkjsa
+//           ;lsakjfd;lsajfd;lksajfd;lkajs;lkfdj;lksajfd;lksaj;lsfd
+//           lksahjlksahdjlkdsa
+//         </p>
+//       </div>
+//     );
+//   }
+//   .topicbox {
+//       position: relative;
+//       min-height: 1px;
+//       padding-right: 16px;
+//       padding-left: 16px;
+//       padding-bottom: 32px;
+//       background: #ffffff;
+//       box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.5);
+//       border-radius: 4px;
+//     }
