78d20b8c6ef87e537df92306ca787d1db63bef01
P0NDER0SA <mikepond11@gmail.com>
2021-04-16 09:08:19 -0400

    adding properties

--- components/organisms/TopicBox.js
+++ components/organisms/TopicBox.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import PropTypes from "prop-types";
 /**
  *  Topic Box Component
  */
@@ -8,28 +9,54 @@ export default function TopicBox(props) {
     <div className="shadow-lg h-96 w-96 rounded-md border-2 pl-2">
       <div className="grid grid-cols-3 grid-flow-row">
         <div className="col-span-2">
-          <h3 className="pt-2 pb-2 font-extrabold">Topic Title</h3>
+          <h3 className="pt-2 pb-2 font-extrabold">{props.title}</h3>
         </div>
         <div className="col-span-1 pt-2">
           <Image
-            src="/images/family.png"
-            alt="icon"
+            src={props.image}
+            alt={props.imgalt}
             width={73}
             height={73}
           ></Image>
         </div>
       </div>
       <br />
-      <p>
-        Topic Content is whateverTopic Content is whateverTopic Content is
-        whateverTopic Content is whatev
-      </p>
+      <p>{props.title}</p>
       <br />
       <p className="font-semibold">Find information about</p>
       <p>thing 1</p>
+      <p>thing 2</p>
+      <p>thing 3</p>
       <br />
       <br />
-      <Link href="/">Explore this journey </Link>
+      <Link href={props.url}>Explore this journey </Link>
     </div>
   );
 }
+
+TopicBox.propTypes = {
+  /**
+   * Titles for topic box tile
+   */
+  title: PropTypes.string.isRequired,
+  /**
+   * Body for topic tile
+   */
+  body: PropTypes.string.isRequired,
+  /**
+   * right corner image for topic box tile
+   */
+  image: PropTypes.string.isRequired,
+  /**
+   * image alt text
+   */
+  imgalt: PropTypes.string.isRequired,
+  /**
+   * link url for topic tile
+   */
+  url: PropTypes.string.isRequired,
+  /**
+   * Test id for cypress test
+   */
+  dataCy: PropTypes.string,
+};
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,46 @@ export default function Home() {
   return (
     <Layout home>
       <div className="flex flex-wrap gap-2 pb-1">
-        <TopicBox />
-        <TopicBox />
-        <TopicBox />
-        <TopicBox />
-        <TopicBox />
+        <TopicBox
+          title="Topic 1"
+          image="/images/family.png"
+          imgalt="alt1"
+          body="Body 1"
+          url="/"
+          dataCy="topic1"
+        />
+        <TopicBox
+          title="Topic 2"
+          image="/images/family.png"
+          imgalt="alt2"
+          body="Body 2"
+          url="/"
+          dataCy="topic2"
+        />
+        <TopicBox
+          title="Topic 3"
+          image="/images/family.png"
+          imgalt="alt3"
+          body="Body 3"
+          url="/"
+          dataCy="topic3"
+        />
+        <TopicBox
+          title="Topic 4"
+          image="/images/family.png"
+          imgalt="alt4"
+          body="Body 4"
+          url="/"
+          dataCy="topic4"
+        />
+        <TopicBox
+          title="Topic 5"
+          image="/images/family.png"
+          imgalt="alt5"
+          body="Body 5"
+          url="/"
+          dataCy="topic5"
+        />
       </div>
     </Layout>
   );
