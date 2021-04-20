2b0d404ea90ddce097a0f21206bd195a3a6f08c8
MarcoGoC <marcogoc951@gmail.com>
2021-04-20 14:23:11 -0400

    fixing the searchbox css and wordmark alignment

--- components/atoms/SearchBar.js
+++ components/atoms/SearchBar.js
@@ -5,15 +5,15 @@ import PropTypes from "prop-types";
  */
 export function SearchBar(props) {
   return (
-    <div className="inline-flex" data-cy={props.dataCy}>
+    <div className="inline-flex searchBox" data-cy={props.dataCy}>
       <form
-        className="inline-flex border border-solid border-gray-light-100 iphone5fix"
+        className="inline-flex w-full searchBox border border-solid border-gray-light-100 "
         onSubmit={props.onSubmit}
       >
         <input
           type="text"
           placeholder={props.placeholder}
-          className="placeholder-text-gray text-text-gray font-body py-1 px-2 w-72 focus:outline-none iphone8fix"
+          className="placeholder-text-gray text-text-gray searchInput font-body py-1 px-2 focus:outline-none"
           onChange={props.onChange}
         />
 
@@ -21,7 +21,7 @@ export function SearchBar(props) {
           title="Search bar button"
           type="submit"
           className={
-            "bg-custom-blue-dark text-white text-center text-base rounded-none pt-0.5 h-full w-10 hover:bg-gray-dark active:bg-gray-dark focus:bg-gray-dark"
+            "bg-custom-blue-dark text-white searchButton text-center text-base rounded-none pt-0.5 h-full  hover:bg-gray-dark active:bg-gray-dark focus:bg-gray-dark"
           }
         >
           <span className={"icon-magnifying"} />
