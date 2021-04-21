81148f2a55a9e168ae5ae1bdb90aab698406d534
MarcoGoC <marcogoc951@gmail.com>
2021-04-21 13:55:55 -0400

    fixing again the searchbox

--- components/atoms/SearchBar.js
+++ components/atoms/SearchBar.js
@@ -5,27 +5,30 @@ import PropTypes from "prop-types";
  */
 export function SearchBar(props) {
   return (
-    <div className="inline-flex searchBox" data-cy={props.dataCy}>
+    <div data-cy={props.dataCy}>
       <form
-        className="inline-flex w-full searchBox border border-solid border-gray-light-100 "
+        className="flex border border-solid border-gray-light-100"
         onSubmit={props.onSubmit}
       >
-        <input
-          type="text"
-          placeholder={props.placeholder}
-          className="placeholder-text-gray text-text-gray searchInput font-body py-1 px-2 focus:outline-none"
-          onChange={props.onChange}
-        />
-
-        <button
-          title="Search bar button"
-          type="submit"
-          className={
-            "bg-custom-blue-dark text-white searchButton text-center text-base rounded-none pt-0.5 h-full  hover:bg-gray-dark active:bg-gray-dark focus:bg-gray-dark"
-          }
-        >
-          <span className={"icon-magnifying"} />
-        </button>
+        <div className="searchBox">
+          <input
+            type="text"
+            placeholder={props.placeholder}
+            className="placeholder-text-gray text-text-gray searchInput font-body py-1 px-2 focus:outline-none"
+            onChange={props.onChange}
+          />
+        </div>
+        <div>
+          <button
+            title="Search bar button"
+            type="submit"
+            className={
+              "bg-custom-blue-dark text-white searchButton text-center text-base rounded-none pt-0.5 h-full  hover:bg-gray-dark active:bg-gray-dark focus:bg-gray-dark"
+            }
+          >
+            <span className={"icon-magnifying"} />
+          </button>
+        </div>
       </form>
     </div>
   );
