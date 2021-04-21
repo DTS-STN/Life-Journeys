68d5d19d0843d7e17936e672d96b5e5268c07e8c
P0NDER0SA <mikepond11@gmail.com>
2021-04-21 15:19:06 -0400

    fixing test

--- components/organisms/TitleSection.test.js
+++ components/organisms/TitleSection.test.js
@@ -8,13 +8,15 @@ expect.extend(toHaveNoViolations);
 describe("TitleSection", () => {
   it("renders page title section", () => {
     const primary = act(() => {
-      render(<TitleSection {...TitleSection.args} />);
+      render(<TitleSection title="test1" description="test2" />);
     });
     expect(primary).toBeTruthy();
   });
 
   it("has no a11y violations", async () => {
-    const { container } = render(<TitleSection {...TitleSection.args} />);
+    const { container } = render(
+      <TitleSection title="test1" description="test2" />
+    );
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
