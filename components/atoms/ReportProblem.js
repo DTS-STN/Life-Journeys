1ff5986d8630949f77d7061887948e49d629f6eb
Kris Charbonneau <char0799@algonquinlive.com>
2021-04-22 12:24:38 -0400

    fixed container, spacing

--- components/atoms/ReportProblem.js
+++ components/atoms/ReportProblem.js
@@ -29,27 +29,29 @@ export default function ReportProblem() {
         <>
           <div className="pt-2">
             <form action="#" onSubmit={onSubmitHandler}>
-              <div className="container border border-gray-md rounded bg-gray-light-report pb-5">
-                <fieldset className="text-left pl-2">
+              <div className="container-sm border border-gray-md rounded bg-gray-light-report pb-5">
+                <fieldset>
                   <legend className="text-center pb-3">
                     <span>
                       <h3>Please select all that apply:</h3>
                     </span>
                   </legend>
-                  <div className="checkbox-input w-15 cursor-pointer appearance-none w-44px h-44px absolute -top-2px -left-2px m-0 z-1">
-                    <label for="problem1">
-                      <input
-                        name="problem1"
-                        id="problem1"
-                        type="checkbox"
-                        value="Yes"
-                      />
-                      &nbsp;A link, button or video is not working
-                    </label>
-                    <input name="problem1" type="hidden" value="No" />
+                  <div className="pl-5">
+                    <div className="checkbox-input w-15 cursor-pointer appearance-none w-44px h-44px absolute -top-2px -left-2px m-0 z-1">
+                      <label for="problem1">
+                        <input
+                          name="problem1"
+                          id="problem1"
+                          type="checkbox"
+                          value="Yes"
+                        />
+                        &nbsp;A link, button or video is not working
+                      </label>
+                      <input name="problem1" type="hidden" value="No" />
+                    </div>
                   </div>
                 </fieldset>
-                <div className="text-left pt-8 text-sm font-display">
+                <div className="text-left pl-2 pt-8 text-sm font-display">
                   <button className="bg-custom-blue-dark pb-1.5 pt-1.5 px-3 ml-2 rounded text-white hover:bg-custom-blue-canadaLight border border-custom-blue-dark">
                     Submit
                   </button>
