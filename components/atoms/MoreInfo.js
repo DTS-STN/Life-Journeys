02314bb479af89057a918f2b26e8ba2cafe1a6ff
Kris Charbonneau <char0799@algonquinlive.com>
2021-04-28 16:42:04 -0400

    Added MoreInfo component and tests

---
+++ components/atoms/MoreInfo.js
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import PropTypes from "prop-types";
+import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+/**
+ *  More Information Component
+ */
+
+export default function MoreInfo(props) {
+  const googleSearch = "https://google.ca/search?q="
+    .concat(props.text)
+    .replace(/\s/g, "+");
+
+  return (
+    <div className="pb-2 pt-2">
+      <div className="pb-2 pt-2 px-3 border border-custom-blue-dark rounded text-custom-blue-dark cursor-pointer focus:ring-1 focus:ring-black focus:ring-offset-1">
+        <Link href={googleSearch}>
+          <a className="block w-full" id={props.id}>
+            <h6>
+              {props.text}
+              <span className="ml-3">
+                <FontAwesomeIcon icon={faArrowAltCircleRight} color="#26374A" />
+              </span>
+            </h6>
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+MoreInfo.propTypes = {
+  /**
+   * Text that will show on the drop down
+   */
+  text: PropTypes.string.isRequired,
+  /**
+   * Identify which element is being clicked
+   */
+  id: PropTypes.string,
+};
--- pages/having-a-child.js
+++ pages/having-a-child.js
@@ -1,10 +1,18 @@
 import Layout from "../components/layout";
+import MoreInfo from "../components/atoms/MoreInfo";
 
 export default function lifejourney() {
   return (
     <Layout
       bannerTitle="Having a child"
       bannerText="Lorem ipsum dolor sit amet, consectetur"
-    ></Layout>
+    >
+      <div>
+        <MoreInfo text="Find community groups in my area" />
+        <MoreInfo text="Find fertility treatment in my area" />
+        <MoreInfo text="Find a health provider in my area" />
+        <MoreInfo text="Find a financial planner in my area" />
+      </div>
+    </Layout>
   );
 }
