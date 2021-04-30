4f7f5fdad07fc3ec711be320a9f22af079c49767
Kris Charbonneau <char0799@algonquinlive.com>
2021-04-30 09:25:44 -0400

    Removed extra div, added links to Accordion

--- pages/having-a-child.js
+++ pages/having-a-child.js
@@ -2,6 +2,7 @@ import Layout from "../components/layout";
 import MoreInfo from "../components/atoms/MoreInfo";
 import { useContext } from "react";
 import { LanguageContext } from "../context/languageProvider";
+import { Accordion } from "../components/atoms/Accordion";
 
 import en from "../locales/en";
 import fr from "../locales/fr";
@@ -16,12 +17,14 @@ export default function lifejourney() {
       bannerTitle={t.havingAChildBannerTitle}
       bannerText={t.havingAChildBannerText}
     >
-      <div>
-        <MoreInfo text={t.moreInfoCommunity} />
-        <MoreInfo text={t.moreInfoFertility} />
-        <MoreInfo text={t.moreInfoHealth} />
-        <MoreInfo text={t.moreInfoFinancial} />
-      </div>
+      <Accordion>
+        <div className="pr-6">
+          <MoreInfo text={t.moreInfoCommunity} />
+          <MoreInfo text={t.moreInfoFertility} />
+          <MoreInfo text={t.moreInfoHealth} />
+          <MoreInfo text={t.moreInfoFinancial} />
+        </div>
+      </Accordion>
     </Layout>
   );
 }
