c537222935afd1a62c373ef8b9f94f858867bf90
MarcoGoC <marcogoc951@gmail.com>
2021-05-03 15:24:47 -0400

    updated having a child page

--- pages/having-a-child.js
+++ pages/having-a-child.js
@@ -25,97 +25,99 @@ export default function lifejourney() {
         <Breadcrumb />
       </div>
 
-      <div className="w-3/12">
-        <SideMenu />
-      </div>
+      <div className="container">
+        <div className="w-3/12 sticky top-0">
+          <SideMenu />
+        </div>
 
-      <div className="w-9/12 float-right">
-        <Accordion
-          id="GettingReady"
-          title="Getting Ready"
-          summary="Getting Ready summary text"
-        >
-          <div className="pr-6">
-            <MoreInfo text={t.moreInfoCommunity} />
-            <MoreInfo text={t.moreInfoFertility} />
-            <MoreInfo text={t.moreInfoHealth} />
-            <MoreInfo text={t.moreInfoFinancial} />
-          </div>
-        </Accordion>
+        <div className="w-9/12 float-right">
+          <Accordion
+            id="GettingReady"
+            title="Getting Ready"
+            summary="Getting Ready summary text"
+          >
+            <div className="pr-6">
+              <MoreInfo text={t.moreInfoCommunity} />
+              <MoreInfo text={t.moreInfoFertility} />
+              <MoreInfo text={t.moreInfoHealth} />
+              <MoreInfo text={t.moreInfoFinancial} />
+            </div>
+          </Accordion>
 
-        <Accordion
-          id="LoremIpsun"
-          title="Lorem Ipsun"
-          summary="Lorem ipsun dolor summary"
-        >
-          <div className="pr-6">
-            <MoreInfo text={t.moreInfoCommunity} />
-            <MoreInfo text={t.moreInfoFertility} />
-            <MoreInfo text={t.moreInfoHealth} />
-            <MoreInfo text={t.moreInfoFinancial} />
-          </div>
-        </Accordion>
+          <Accordion
+            id="LoremIpsun"
+            title="Lorem Ipsun"
+            summary="Lorem ipsun dolor summary"
+          >
+            <div className="pr-6">
+              <MoreInfo text={t.moreInfoCommunity} />
+              <MoreInfo text={t.moreInfoFertility} />
+              <MoreInfo text={t.moreInfoHealth} />
+              <MoreInfo text={t.moreInfoFinancial} />
+            </div>
+          </Accordion>
 
-        <Accordion
-          id="WhenArrives"
-          title="When your child arrives"
-          summary="When your child arrives summary text"
-        >
-          <div className="pr-6">
-            <MoreInfo text={t.moreInfoCommunity} />
-            <MoreInfo text={t.moreInfoFertility} />
-            <MoreInfo text={t.moreInfoHealth} />
-            <MoreInfo text={t.moreInfoFinancial} />
-          </div>
-        </Accordion>
+          <Accordion
+            id="WhenArrives"
+            title="When your child arrives"
+            summary="When your child arrives summary text"
+          >
+            <div className="pr-6">
+              <MoreInfo text={t.moreInfoCommunity} />
+              <MoreInfo text={t.moreInfoFertility} />
+              <MoreInfo text={t.moreInfoHealth} />
+              <MoreInfo text={t.moreInfoFinancial} />
+            </div>
+          </Accordion>
 
-        <Accordion
-          id="Consectur"
-          title="Consectur"
-          summary="Consectur summary text"
-        >
-          <div className="pr-6">
-            <MoreInfo text={t.moreInfoCommunity} />
-            <MoreInfo text={t.moreInfoFertility} />
-            <MoreInfo text={t.moreInfoHealth} />
-            <MoreInfo text={t.moreInfoFinancial} />
-          </div>
-        </Accordion>
+          <Accordion
+            id="Consectur"
+            title="Consectur"
+            summary="Consectur summary text"
+          >
+            <div className="pr-6">
+              <MoreInfo text={t.moreInfoCommunity} />
+              <MoreInfo text={t.moreInfoFertility} />
+              <MoreInfo text={t.moreInfoHealth} />
+              <MoreInfo text={t.moreInfoFinancial} />
+            </div>
+          </Accordion>
 
-        <Accordion
-          id="FinancialSupport"
-          title="Financial Support"
-          summary="Financial Support summary text"
-        >
-          <div className="pr-6">
-            <MoreInfo text={t.moreInfoCommunity} />
-            <MoreInfo text={t.moreInfoFertility} />
-            <MoreInfo text={t.moreInfoHealth} />
-            <MoreInfo text={t.moreInfoFinancial} />
-          </div>
-        </Accordion>
+          <Accordion
+            id="FinancialSupport"
+            title="Financial Support"
+            summary="Financial Support summary text"
+          >
+            <div className="pr-6">
+              <MoreInfo text={t.moreInfoCommunity} />
+              <MoreInfo text={t.moreInfoFertility} />
+              <MoreInfo text={t.moreInfoHealth} />
+              <MoreInfo text={t.moreInfoFinancial} />
+            </div>
+          </Accordion>
 
-        <Accordion
-          id="DolorConsectetur"
-          title="Dolor Consectetur"
-          summary="Dolor Consectetur summary text"
-        >
-          <div className="pr-6">
-            <MoreInfo text={t.moreInfoCommunity} />
-            <MoreInfo text={t.moreInfoFertility} />
-            <MoreInfo text={t.moreInfoHealth} />
-            <MoreInfo text={t.moreInfoFinancial} />
-          </div>
-        </Accordion>
+          <Accordion
+            id="DolorConsectetur"
+            title="Dolor Consectetur"
+            summary="Dolor Consectetur summary text"
+          >
+            <div className="pr-6">
+              <MoreInfo text={t.moreInfoCommunity} />
+              <MoreInfo text={t.moreInfoFertility} />
+              <MoreInfo text={t.moreInfoHealth} />
+              <MoreInfo text={t.moreInfoFinancial} />
+            </div>
+          </Accordion>
 
-        <Accordion id="Ipsum" title="Ipsum" summary="Ipsum summary text">
-          <div className="pr-6">
-            <MoreInfo text={t.moreInfoCommunity} />
-            <MoreInfo text={t.moreInfoFertility} />
-            <MoreInfo text={t.moreInfoHealth} />
-            <MoreInfo text={t.moreInfoFinancial} />
-          </div>
-        </Accordion>
+          <Accordion id="Ipsum" title="Ipsum" summary="Ipsum summary text">
+            <div className="pr-6">
+              <MoreInfo text={t.moreInfoCommunity} />
+              <MoreInfo text={t.moreInfoFertility} />
+              <MoreInfo text={t.moreInfoHealth} />
+              <MoreInfo text={t.moreInfoFinancial} />
+            </div>
+          </Accordion>
+        </div>
       </div>
     </Layout>
   );
