3793527d8eee7f448518ca2802c6559144c8e744
MarcoGoC <marcogoc951@gmail.com>
2021-05-26 15:03:31 -0400

    fetching data from AEM

--- pages/api/topics.js
+++ pages/api/topics.js
@@ -1,5 +1,21 @@
-import { topics } from "../api/data";
+//  Fetch calls to AEM
 
-export default function handler(req, res) {
-  res.status(200).json(topics);
+//const { NEXTJS_ADOBE_API_KEY, NEXTJS_ADOBE_URL } = process.env
+const NEXTJS_ADOBE_URL = process.env.NEXTJS_ADOBE_URL
+  ? process.env.NEXTJS_ADOBE_URL
+  : "https://www.canada.ca/api/assets/decd-endc/content-fragments.json";
+
+//
+// Fetch ALL topics from Adobe
+//
+
+export async function getTopics(locale) {
+  const lang = locale;
+  //const res = await fetch(`${NEXTJS_ADOBE_URL}/content/posts/?key=${NEXTJS_ADOBE_API_KEY}&filter=tag:${lang}`)
+  const res = await fetch(`${NEXTJS_ADOBE_URL}`);
+
+  const errorCode = res.ok ? false : res.statusCode;
+  const topicsData = await res.json();
+
+  return { topicsData, errorCode };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,53 @@
 import TopicBox from "../components/molecules/TopicBox";
 import Layout from "../components/layout";
-import userSwr from "swr";
-import { useContext } from "react";
+import { getTopics } from "./api/topics";
+import { useContext, useEffect, useState } from "react";
 import { LanguageContext } from "../context/languageProvider";
 import Breadcrumb from "../components/molecules/Breadcrumb";
 
 import en from "../locales/en";
 import fr from "../locales/fr";
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
-
 export default function Home() {
   const { items } = useContext(LanguageContext);
   const language = items.language;
   const t = language === "en" ? en : fr;
 
-  const { data, error } = userSwr("/api/topics", fetcher);
+  const [topicsData, setTopicData] = useState(null);
+  const [errorCode, setErrorCode] = useState(false);
 
-  if (!data) return <div>Loading...</div>;
-  if (error === !null) return <div>Failed to load topics...</div>;
+  useEffect(() => {
+    async function TopicList() {
+      const { errorCode, topicsData } = await getTopics("en");
+
+      let topics = [];
+
+      topicsData.entities.map((entity) => {
+        if (entity.properties.contentFragment) {
+          topics.push({
+            title: entity.properties.elements.title.value,
+            body: entity.properties.elements.description.value,
+            image: "/images/family.png",
+            imgalt: "Alt text TBD",
+            url: entity.properties.elements.link.value,
+            subtopics: entity.properties.elements.tags.value
+              .toString()
+              .split(","),
+          });
+        }
+      });
+
+      // console.log("topics = ", topics)
+      if (errorCode) setErrorCode(true);
+      setTopicData(topics);
+    }
+    TopicList();
+  }, []);
+
+  if (!topicsData)
+    return <div className="center text-h3">Loading Data ...</div>;
+
+  if (errorCode) return <div>Failed to load topics...</div>;
 
   return (
     <Layout home>
@@ -37,19 +66,21 @@ export default function Home() {
       </div>
       <div className="container flex flex-wrap">
         <div className="mt-4 mb-4 grid lg:grid-cols-3 sm:grid-cols-2 grid-flow-row gap-3 md:gap-10">
-          {data.map(function (d, idx) {
-            return (
-              <TopicBox
-                key={idx}
-                title={d.title}
-                body={d.body}
-                image={d.image}
-                imgalt={d.imgalt}
-                url={d.url}
-                subtopics={d.subtopics}
-              />
-            );
-          })}
+          {topicsData.map(
+            ({ title, body, image, imgalt, url, subtopics }, idx) => {
+              return (
+                <TopicBox
+                  key={idx}
+                  title={title}
+                  body={body}
+                  image={image}
+                  imgalt={imgalt}
+                  url={url}
+                  subtopics={subtopics}
+                />
+              );
+            }
+          )}
         </div>
       </div>
     </Layout>
