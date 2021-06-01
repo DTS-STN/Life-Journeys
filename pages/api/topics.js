870d66887800d0f37126ecdbb581a78a9b910d0a
MarcoGoC <marcogoc951@gmail.com>
2021-06-01 12:05:18 -0400

    updated with Kris comment

--- pages/api/topics.js
+++ pages/api/topics.js
@@ -6,8 +6,6 @@ import path from "path";
 //
 
 export function getLocalData() {
-  //const dataDirectory = path.join(process.cwd(), "pages/api");
-  //const fullPath = path.join(process.cwd(), "pages", "api", "data.json");
   const fullPath = path.join(process.cwd(), "pages", "api", "data.json");
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const localData = JSON.parse(fileContents).topics;
