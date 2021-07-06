913973104ee5b5adea646183319bfcc4eacfa31a
MarcoGoC <marcogoc951@gmail.com>
2021-07-06 13:34:49 -0400

    gets cards in english or french

--- pages/having-a-baby.js
+++ pages/having-a-baby.js
@@ -18,152 +18,170 @@ export default function lifejourney() {
   const t = language === "en" ? en : fr;
 
   return (
-    <Layout title={t.havingABabyTitle}>
-      <Banner
-        siteTitle={t.havingAChildBannerTitle}
-        headline={t.havingAChildBannerText}
-      />
-      <div id="wb-bc" className="container my-6">
-        <Breadcrumb />
-      </div>
-
-      <div className="container flex flex-col md:flex-row align-items-center ">
-        <div className="pr-3 w-full lg:w-3/12 top-0 lg:sticky ">
-          <SideMenu />
+    <>
+      <Layout title={t.havingABabyTitle}>
+        <Banner
+          siteTitle={t.havingAChildBannerTitle}
+          headline={t.havingAChildBannerText}
+        />
+        <div id="wb-bc" className="container my-6">
+          <Breadcrumb />
         </div>
 
-        <div className="pt-2 w-full lg:w-9/12">
-          <Accordion
-            id="GettingReady"
-            title="Getting Ready"
-            summary="Getting Ready summary text"
-          >
-            <div className="pr-6">
-              <Table />
+        <section className="layout-container mb-2 mt-4">
+          <div className="container flex flex-col md:flex-row align-items-center ">
+            <div className="pr-3 w-full lg:w-3/12 top-0 lg:sticky ">
+              <SideMenu />
             </div>
-            <div className="py-6">
-              <p className="text-h4 font-bold font-display">{t.getConnected}</p>
-              <div className="pr-6">
-                <MoreInfo text={t.moreInfoCommunity} />
-                <MoreInfo text={t.moreInfoFertility} />
-                <MoreInfo text={t.moreInfoHealth} />
-                <MoreInfo text={t.moreInfoFinancial} />
-                <AvailableResources title={t.gettingready} />
-              </div>
-            </div>
-          </Accordion>
 
-          <Accordion
-            id="LoremIpsun"
-            title="Lorem Ipsun"
-            summary="Lorem ipsun dolor summary"
-          >
-            <div className="pr-6">
-              <Table />
-            </div>
-            <div className="py-6">
-              <p className="text-h4 font-bold font-display">{t.getConnected}</p>
-              <div className="pr-6">
-                <MoreInfo text={t.moreInfoCommunity} />
-                <MoreInfo text={t.moreInfoFertility} />
-                <MoreInfo text={t.moreInfoHealth} />
-                <MoreInfo text={t.moreInfoFinancial} />
-              </div>
-            </div>
-          </Accordion>
+            <div className="pt-2 w-full lg:w-9/12">
+              <Accordion
+                id="GettingReady"
+                title="Getting Ready"
+                summary="Getting Ready summary text"
+              >
+                <div className="pr-6">
+                  <Table />
+                </div>
+                <div className="py-6">
+                  <p className="text-h4 font-bold font-display">
+                    {t.getConnected}
+                  </p>
+                  <div className="pr-6">
+                    <MoreInfo text={t.moreInfoCommunity} />
+                    <MoreInfo text={t.moreInfoFertility} />
+                    <MoreInfo text={t.moreInfoHealth} />
+                    <MoreInfo text={t.moreInfoFinancial} />
+                    <AvailableResources title={t.gettingready} />
+                  </div>
+                </div>
+              </Accordion>
 
-          <Accordion
-            id="WhenArrives"
-            title="When your child arrives"
-            summary="When your child arrives summary text"
-          >
-            <div className="pr-6">
-              <Table />
-            </div>
-            <div className="py-6">
-              <p className="text-h4 font-bold font-display">{t.getConnected}</p>
-              <div className="pr-6">
-                <MoreInfo text={t.moreInfoCommunity} />
-                <MoreInfo text={t.moreInfoFertility} />
-                <MoreInfo text={t.moreInfoHealth} />
-                <MoreInfo text={t.moreInfoFinancial} />
-              </div>
-            </div>
-          </Accordion>
+              <Accordion
+                id="LoremIpsun"
+                title="Lorem Ipsun"
+                summary="Lorem ipsun dolor summary"
+              >
+                <div className="pr-6">
+                  <Table />
+                </div>
+                <div className="py-6">
+                  <p className="text-h4 font-bold font-display">
+                    {t.getConnected}
+                  </p>
+                  <div className="pr-6">
+                    <MoreInfo text={t.moreInfoCommunity} />
+                    <MoreInfo text={t.moreInfoFertility} />
+                    <MoreInfo text={t.moreInfoHealth} />
+                    <MoreInfo text={t.moreInfoFinancial} />
+                  </div>
+                </div>
+              </Accordion>
 
-          <Accordion
-            id="Consectur"
-            title="Consectur"
-            summary="Consectur summary text"
-          >
-            <div className="pr-6">
-              <Table />
-            </div>
-            <div className="py-6">
-              <p className="text-h4 font-bold font-display">{t.getConnected}</p>
-              <div className="pr-6">
-                <MoreInfo text={t.moreInfoCommunity} />
-                <MoreInfo text={t.moreInfoFertility} />
-                <MoreInfo text={t.moreInfoHealth} />
-                <MoreInfo text={t.moreInfoFinancial} />
-              </div>
-            </div>
-          </Accordion>
+              <Accordion
+                id="WhenArrives"
+                title="When your child arrives"
+                summary="When your child arrives summary text"
+              >
+                <div className="pr-6">
+                  <Table />
+                </div>
+                <div className="py-6">
+                  <p className="text-h4 font-bold font-display">
+                    {t.getConnected}
+                  </p>
+                  <div className="pr-6">
+                    <MoreInfo text={t.moreInfoCommunity} />
+                    <MoreInfo text={t.moreInfoFertility} />
+                    <MoreInfo text={t.moreInfoHealth} />
+                    <MoreInfo text={t.moreInfoFinancial} />
+                  </div>
+                </div>
+              </Accordion>
 
-          <Accordion
-            id="FinancialSupport"
-            title="Financial Support"
-            summary="Financial Support summary text"
-          >
-            <div className="pr-6">
-              <Table />
-            </div>
-            <div className="py-6">
-              <p className="text-h4 font-bold font-display">{t.getConnected}</p>
-              <div className="pr-6">
-                <MoreInfo text={t.moreInfoCommunity} />
-                <MoreInfo text={t.moreInfoFertility} />
-                <MoreInfo text={t.moreInfoHealth} />
-                <MoreInfo text={t.moreInfoFinancial} />
-              </div>
-            </div>
-          </Accordion>
+              <Accordion
+                id="Consectur"
+                title="Consectur"
+                summary="Consectur summary text"
+              >
+                <div className="pr-6">
+                  <Table />
+                </div>
+                <div className="py-6">
+                  <p className="text-h4 font-bold font-display">
+                    {t.getConnected}
+                  </p>
+                  <div className="pr-6">
+                    <MoreInfo text={t.moreInfoCommunity} />
+                    <MoreInfo text={t.moreInfoFertility} />
+                    <MoreInfo text={t.moreInfoHealth} />
+                    <MoreInfo text={t.moreInfoFinancial} />
+                  </div>
+                </div>
+              </Accordion>
 
-          <Accordion
-            id="DolorConsectetur"
-            title="Dolor Consectetur"
-            summary="Dolor Consectetur summary text"
-          >
-            <div className="pr-6">
-              <Table />
-            </div>
-            <div className="py-6">
-              <p className="text-h4 font-bold font-display">{t.getConnected}</p>
-              <div className="pr-6">
-                <MoreInfo text={t.moreInfoCommunity} />
-                <MoreInfo text={t.moreInfoFertility} />
-                <MoreInfo text={t.moreInfoHealth} />
-                <MoreInfo text={t.moreInfoFinancial} />
-              </div>
-            </div>
-          </Accordion>
+              <Accordion
+                id="FinancialSupport"
+                title="Financial Support"
+                summary="Financial Support summary text"
+              >
+                <div className="pr-6">
+                  <Table />
+                </div>
+                <div className="py-6">
+                  <p className="text-h4 font-bold font-display">
+                    {t.getConnected}
+                  </p>
+                  <div className="pr-6">
+                    <MoreInfo text={t.moreInfoCommunity} />
+                    <MoreInfo text={t.moreInfoFertility} />
+                    <MoreInfo text={t.moreInfoHealth} />
+                    <MoreInfo text={t.moreInfoFinancial} />
+                  </div>
+                </div>
+              </Accordion>
 
-          <Accordion id="Ipsum" title="Ipsum" summary="Ipsum summary text">
-            <div className="pr-6">
-              <Table />
+              <Accordion
+                id="DolorConsectetur"
+                title="Dolor Consectetur"
+                summary="Dolor Consectetur summary text"
+              >
+                <div className="pr-6">
+                  <Table />
+                </div>
+                <div className="py-6">
+                  <p className="text-h4 font-bold font-display">
+                    {t.getConnected}
+                  </p>
+                  <div className="pr-6">
+                    <MoreInfo text={t.moreInfoCommunity} />
+                    <MoreInfo text={t.moreInfoFertility} />
+                    <MoreInfo text={t.moreInfoHealth} />
+                    <MoreInfo text={t.moreInfoFinancial} />
+                  </div>
+                </div>
+              </Accordion>
+
+              <Accordion id="Ipsum" title="Ipsum" summary="Ipsum summary text">
+                <div className="pr-6">
+                  <Table />
+                </div>
+                <div className="py-6">
+                  <p className="text-h4 font-bold font-display">
+                    {t.getConnected}
+                  </p>
+                  <div className="pr-6">
+                    <MoreInfo text={t.moreInfoCommunity} />
+                    <MoreInfo text={t.moreInfoFertility} />
+                    <MoreInfo text={t.moreInfoHealth} />
+                    <MoreInfo text={t.moreInfoFinancial} />
+                  </div>
+                </div>
+              </Accordion>
             </div>
-            <div className="py-6">
-              <p className="text-h4 font-bold font-display">{t.getConnected}</p>
-              <div className="pr-6">
-                <MoreInfo text={t.moreInfoCommunity} />
-                <MoreInfo text={t.moreInfoFertility} />
-                <MoreInfo text={t.moreInfoHealth} />
-                <MoreInfo text={t.moreInfoFinancial} />
-              </div>
-            </div>
-          </Accordion>
-        </div>
-      </div>
-    </Layout>
+          </div>
+        </section>
+      </Layout>
+    </>
   );
 }
