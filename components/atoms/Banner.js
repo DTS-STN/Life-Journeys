e4673ca0a1f472a27774012b85166e1b4197ff84
Kris Charbonneau <char0799@algonquinlive.com>
2021-08-04 13:50:26 -0400

    added h1 back to page

--- components/atoms/Banner.js
+++ components/atoms/Banner.js
@@ -13,9 +13,9 @@ export const Banner = ({ headline }) => {
         className="bg-cover bg-no-repeat xxs:bg-center bg-right h-full md:bg-banner-img md:py-10 xxs:bg-banner-img-mobile"
       >
         <div className="lg:container lg:px-8 xxl:mx-auto xxs:pl-4 lg:py-10 xxs:pt-8">
-          <p className="md:text-h1l text-h1 font-bold font-display py-20 md:py-8 break-words xxs:pb-4">
+          <h1 className="md:text-h1l text-h1 font-bold font-display py-20 md:py-8 break-words xxs:pb-4">
             {headline}
-          </p>
+          </h1>
         </div>
       </div>
     </div>
