bd344f18abec609123e63cfbd7dfc36f446e8aea
Kyle Meelker <kyle.meelker@gmail.com>
2021-09-02 12:37:06 -0400

    remove unused import

--- components/atoms/PhaseBanner.js
+++ components/atoms/PhaseBanner.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { ActionButton } from "./ActionButton";
 import { useState } from "react";
